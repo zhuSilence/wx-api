80bdcf52eaad526b766c17b817d54345d28c46c9
silence <zhuxiang134@gmail.com>
2023-05-07 21:49:59 +0800

    fix

--- src/main/java/com/github/niefy/modules/wx/service/impl/MsgReplyDefaultServiceImpl.java
+++ src/main/java/com/github/niefy/modules/wx/service/impl/MsgReplyDefaultServiceImpl.java
@@ -58,7 +58,7 @@ public class MsgReplyDefaultServiceImpl implements MsgReplyDefaultService {
             }
             //替换关注回复中的 ${openId}
             //String replyContent = rules.get(0).getReplyContent().replace("${OPEN_ID}", requestContext.getToUser());
-            return this.reply(requestContext.getToUser(), requestContext.getFromUser(), requestContext.getReplyType(), requestContext.getResponseContent());
+            return this.reply(requestContext.getFromUser(), requestContext.getToUser(), requestContext.getReplyType(), requestContext.getResponseContent());
         } catch (Exception e) {
             log.error("自动回复出错：", e);
         }
